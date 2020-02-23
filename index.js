1d0c9a535d01c4284be9ce09f68cd9076e73663f
Robert Kempton <r@snow87.com>
2020-02-22 19:20:01 -0700

    make withDb async because it is

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const db = require( './src/db' )
 module.exports = {
     init: db.init,
     db: db.get,
-    withDb( fun ) {
+    async withDb( fun ) {
         return async function() {
             return fun( await db.get(), ...arguments )
         }
