3f1e1fedf079224bf7cd1a984f6aa3b535b623b6
Robert Kempton <r@snow87.com>
2020-02-22 19:20:59 -0700

    actually no

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const db = require( './src/db' )
 module.exports = {
     init: db.init,
     db: db.get,
-    async withDb( fun ) {
+    withDb( fun ) {
         return async function() {
             return fun( await db.get(), ...arguments )
         }
