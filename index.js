8cb2ff7f3befd8425dfd83ba9682aa95c454da8d
Robert Kempton <r@snow87.com>
2020-02-22 21:16:37 -0700

    add newId method to get new objectIds without having to bring in mongodb

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const db = require( './src/db' )
+const {ObjectId} = require('mongodb')
 module.exports = {
     init: db.init,
     db: db.get,
+    newId: ()=>new ObjectId().toString(),
     withDb( fun ) {
         return async function() {
             return fun( await db.get(), ...arguments )
